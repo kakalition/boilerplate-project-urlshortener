94c1260d8c94249b71091fdb424c0d4b596a5834
Kaka <kakalition@gmail.com>
2024-07-22 18:48:58 +0700

    patch

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ const express = require('express');
 const cors = require('cors');
 
 var bodyParser = require('body-parser')
-var jsonParser = bodyParser.json()
-
 const app = express();
 
 const map = new Map();
@@ -13,7 +11,7 @@ const map = new Map();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
-
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use('/public', express.static(`${process.cwd()}/public`));
 
 app.get('/', function (req, res) {
@@ -25,8 +23,16 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
-app.post('/api/shorturl', jsonParser, function (req, res) {
+app.post('/api/shorturl', function (req, res) {
   const url = req.body.url;
+  console.log('url', url)
+
+  if (!url) {
+    return res.json({
+      error: 'invalid_url',
+    })
+  }
+
   if (!(url.startsWith('http://www.') || url.startsWith('https://www.')) || !url.endsWith('.com')) {
     return res.json({
       error: 'invalid_url',
